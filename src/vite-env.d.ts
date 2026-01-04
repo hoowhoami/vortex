b08d5e48413f7b33d63a9df79478fb5def9fd286
Jiangliangtong <jiangliangtong@gmail.com>
2026-01-04 20:28:21 +0800

    feat: reset

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface Window {
-  __TAURI__?: {
-    core: {
-      invoke: (cmd: string, args?: Record<string, unknown>) => Promise<unknown>;
-    };
-  };
-}
