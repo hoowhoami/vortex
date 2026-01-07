53f111cf9903c9479afe9823bf10108dbd533118
jiangliangtong <jiangliangtong@gmail.com>
2026-01-07 15:54:43 +0800

    feat: reset

--- src/app/douban/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const dynamic = 'force-dynamic';
-
-export default function DoubanLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
-}
--- src/app/home/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const dynamic = 'force-dynamic';
-
-export default function HomeLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
-}
